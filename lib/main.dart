a062ad4f57866a6d976426fc9cdf69dd4c3f5f81
azzamjafri <azzamjafri98@gmail.com>
2019-07-27 15:05:35 +0530

    material App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './ui/welcome_home.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,41 +7,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
 
-      home: Material(
-
-        color: Colors.lightBlueAccent,
-
-        child: Center(
-          child: Text(
-            "Hello Material !",
-            textDirection: TextDirection.ltr,
-            style: new TextStyle(fontWeight: FontWeight.w800, fontStyle: FontStyle.italic),
-          ),
-        )
-
-
-      ),
-
-
-
-
-//      title: 'Welcome to Flutter',
-//      home: Scaffold(
-//        appBar: AppBar(
-//          title: Text('First Flutter App'),
-//        ),
-//        body: Center(
-//          child: Text('Hello World'),
-//        ),
-//      ),
-
-//      home: new Text(
-//        "hello, there",
-//        textDirection: TextDirection.ltr,
-//      )
+      title: "Welcome APP !",
+      home: new Welcome(),
 
     );
   }
 }
+
